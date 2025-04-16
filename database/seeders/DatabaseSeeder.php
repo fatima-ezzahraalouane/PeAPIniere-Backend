7f52110f7d5c18e1ce804196f2c94c825d6292e3
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-16 14:53:01 +0100

    Ajout de UserSeeder au DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             PlantSeeder::class,
             ImageSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
