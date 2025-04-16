48fd8e0ad2a6fa795fa114029c87703364500167
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-16 14:44:39 +0100

    Ajout de descriptions aux catégories dans le seeder de la base de données

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -14,8 +12,24 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        Category::create(['name' => 'Plantes Aromatiques']);
-        Category::create(['name' => 'Plantes Médicinales']);
-        Category::create(['name' => 'Plantes Décoratives']);
+        Category::create([
+            'name' => 'Plantes Aromatiques',
+            'description' => 'Plantes utilisées pour parfumer les plats, comme le basilic, la menthe ou le thym.'
+        ]);
+
+        Category::create([
+            'name' => 'Plantes Médicinales',
+            'description' => 'Plantes utilisées pour leurs bienfaits sur la santé, en phytothérapie ou en infusion.'
+        ]);
+
+        Category::create([
+            'name' => 'Plantes Décoratives',
+            'description' => 'Plantes choisies pour embellir les espaces intérieurs ou extérieurs.'
+        ]);
+
+        Category::create([
+            'name' => 'Arbres et Arbustes',
+            'description' => 'Végétaux plus grands pour structurer un jardin, créer de l’ombre ou des haies.'
+        ]);
     }
 }
